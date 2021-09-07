6091291278573bbcd27efe6ecf843c56aad767b0
Martin Jahoda <jahodaa.m93@gmail.com>
2021-09-07 14:11:24 +0100

    added new project

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,15 +17,34 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img:'profile-pic.jpg',
-  paragraphOne: 'I am self though front-end developer who is passionate about building awesome website’s and application’s! I have currently learned and used HTML, CSS, SCSS and Vanilla Javascript. I am currently learning React and I can’t wait to build a new website’s with it!. On personal level, I am self-driven & hard working, I am able to adapt very quick and overall I am very curious. Also, I am really friendly who can fit in any team. ',
-   paragraphTwo: 'My current skills',
-   paragraphThree: 'HTML, CSS, BOOTSTRAP, SASS, JAVASCRIPT ',
+  img: 'profile-pic.jpg',
+  paragraphOne:
+    'I am self though front-end developer who is passionate about building awesome website’s and application’s! I have currently learned and used HTML, CSS, SASS and Vanilla Javascript. I am currently learning React and I can’t wait to build a new website’s with it!. On personal level, I am self-driven & hard working, I am able to adapt very quick and overall I am very curious. Also, I am really friendly who can fit in any team. ',
+  paragraphTwo: 'My current skills',
+  paragraphThree: 'HTML, CSS, BOOTSTRAP, SASS, JAVASCRIPT ',
   resume: 'https://drive.google.com/drive/my-drive', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'Project4.png',
+    title: 'To Do List',
+    info: ' I have built To Do List in order to get better with Javascript. You can add tasks and remove it all together with Delete All or just one by one with a button remove. I have also implemented local storage for this project. ',
+    // info2: '',
+    url: 'https://baiid.github.io/ToDoList/',
+    repo: 'https://github.com/Baiid/ToDoList', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Project3.png',
+    title: 'Weather App',
+    info: 'This is a responsive weather app made with Javascript using open-weather API. It is my first project using API.I will dive into API more in the future',
+    // info2: '',
+    url: 'https://baiid.github.io/Weather-app/',
+    repo: 'https://github.com/Baiid/Weather-app', // if no repo, the button will not show up
+  },
   {
     id: nanoid(),
     img: 'Project1.png',
@@ -44,15 +63,6 @@ export const projectsData = [
     url: 'https://baiid.github.io/Email-Template/',
     repo: 'https://github.com/Baiid/Email-Template', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'Project3.png',
-    title: 'Weather App',
-    info: 'This is a responsive weather app made with Javascript using open-weather API. It is my first project using API.I will dive into API more in the future',
-    // info2: '',
-    url: 'https://baiid.github.io/Weather-app/',
-    repo: 'https://github.com/Baiid/Weather-app', // if no repo, the button will not show up
-  },
 ];
 
 // CONTACT DATA
@@ -60,14 +70,13 @@ export const contactData = {
   cta: '',
   btn: 'Email: mjahodax@gmail.com',
   email: 'mjahodax@gmail.com',
-  btn2:'Phone number: 07859513452',
-  tel:'07859513452',
+  btn2: 'Phone number: 07859513452',
+  tel: '07859513452',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-
     {
       id: nanoid(),
       name: 'linkedin',
